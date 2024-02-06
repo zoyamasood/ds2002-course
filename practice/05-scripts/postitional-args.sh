9cd92fda4bf799025817f731f41c57538039bb28
Neal Magee <nem2p@virginia.edu>
2024-02-06 15:22:21 -0500

    Stub for class

--- practice/05-scripts/postitional-args.sh
+++ practice/05-scripts/postitional-args.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Positional arguments are zero-indexed, starting with the script itself:
-
-echo "$0 <-- This is the 0 argument"
-echo "$1 <-- This is the 1 argument"
+#
+# ./myscript.sh              <-- this would be the 0 argument
+# ./myscript.sh subcommand   <-- "subcommand" would be the 1 argument
+# ... and so on ...
+#
 
