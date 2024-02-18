756445e3c7cbdc63877519f3c931d7b9d649d288
zoyamasood <156849553+zoyamasood@users.noreply.github.com>
2024-02-18 15:45:47 -0500

    mad lib for lab3

---
+++ mywork/lab3/madlib.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+clear 
+echo "Let's build a mad-lib!" 
+
+read -p "1. Please give me an adjective: " ADJ1
+read -p "2. Please give me a silly word: " SLW 
+read -p "3. Please give me an animal: " ANI 
+read -p "4. Please give me another adjective: " ADJ2
+read -p "5. Please give me a noun: " NOU
+read -p "6. Please give me a past tense verb: " VER
+read -p "7. Please give me a body part: " BDP
+read -p "8. Please give me an adverb ending in -ly: " ADV
+
+echo "Once upon a time, in a kingdom far, far away, there lived a $ADJ1 princess. Princess $SLW was loved by everyone in the Kingdom of $ANI-shire. One night, something $ADJ2 happened that changed Princess $SLW's life forever. A $NOU breathing dragon attacked the castle! All the guards $VER with the dragon but it was too strong. Suddendly, a Knight appeared and stabbed the dragon's $BDP with his sword, saving the princess! Princess $SLW was so happy she married the Knight and they all lived $ADV ever after!" 
+ 
